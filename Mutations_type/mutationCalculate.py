975790679447f12e46cd29dda69376f3c2fc00ba
wishyoulikebefore <37919975+wishyoulikebefore@users.noreply.github.com>
2018-04-09 13:52:43 +0800

    Update mutationCalculate.py

--- Mutations_type/mutationCalculate.py
+++ Mutations_type/mutationCalculate.py
@@ -28,90 +28,92 @@ def MutationProcess(strain):
     resistantFile = open(strain+".txt").readlines()
     mutationDict = defaultdict(set)
     for row in resistantFile:
-        rowOfList = row.rstrip().split()
-        mutation = rowOfList[1]+"_"+rowOfList[5]
-        if "RIF" in row:
-            mutationDict["RIF"].add(mutation)
-        elif "ISO" in row:
-            mutationDict["INH"].add(mutation)
-        elif "FLUOROQUINOLONES" in row:
-            mutationDict["FLUO"].add(mutation)
-        elif "AMIKACIN" in row or "CAPREOMYCIN" in row or "KANAMYCIN" in row:
-            mutationDict["ACK"].add(mutation)
-        elif "ETHAMBUTOL" in row:
-            mutationDict["EMB"].add(mutation)
-        elif "PYRAZINAMIDE" in row:
-            mutationDict["PZA"].add(mutation)
-        else:
+        if "lineage" in row:
             pass
+        else:
+            rowOfList = row.rstrip().split()
+            mutation = rowOfList[1]+"_"+rowOfList[5]
+            if "RIF" in row:
+                mutationDict["RIF"].add(mutation)
+            elif "ISO" in row:
+                mutationDict["INH"].add(mutation)
+            elif "FLUOROQUINOLONES" in row:
+                mutationDict["FLUO"].add(mutation)
+            elif "AMIKACIN" in row or "CAPREOMYCIN" in row or "KANAMYCIN" in row:
+                mutationDict["ACK"].add(mutation)
+            elif "ETHAMBUTOL" in row:
+                mutationDict["EMB"].add(mutation)
+            elif "PYRAZINAMIDE" in row:
+                mutationDict["PZA"].add(mutation)
+            else:
+                pass
+    period = judgeResistancePeriod(mutationDict)
+    distribute(strain,period,mutationDict)
     RIF = ["+".join(mutationDict.get("RIF")) if mutationDict.get("RIF") else ""]
     INH = ["+".join(mutationDict.get("INH")) if mutationDict.get("INH") else ""]
     ACK = ["+".join(mutationDict.get("ACK")) if mutationDict.get("ACK") else ""]
     FLUO = ["+".join(mutationDict.get("FLUO")) if mutationDict.get("FLUO") else ""]
     EMB = ["+".join(mutationDict.get("EMB")) if mutationDict.get("EMB") else ""]
     PZA = ["+".join(mutationDict.get("PZA")) if mutationDict.get("PZA") else ""]
-    period = judgeResistancePeriod(INH,RIF,ACK,FLUO,EMB,PZA)
     output.write("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n" %(strain,INH,RIF,ACK,FLUO,EMB,PZA,period))
-    distribute(strain,period,INH,RIF,ACK,FLUO,EMB,PZA)
     IREP_calculate(strain,period,INH,RIF,EMB,PZA)
     print("%s.txt has been processed" % (strain))
 
-def judgeResistancePeriod(INH,RIF,ACK,FLUO,EMB,PZA):
-    if RIF != "" and INH != "":
-        if FLUO == "" and ACK == "":
-            return "MDR"
-        elif FLUO != "" and ACK != "":
+def judgeResistancePeriod(mutationDict):
+    if mutationDict.get("RIF") and mutationDict.get("INH"):
+        if mutationDict.get("FLUO") and mutationDict.get("ACK"):
             return "XDR"
-        else:
+        elif mutationDict.get("FLUO") or mutationDict.get("ACK"):
             return "pre-XDR"
-    elif FLUO == "" and ACK == "" and INH == "" and RIF == "" and PZA == "" and EMB == "":
-        return "S"
-    else:
+        else:
+            return "MDR"
+    elif mutationDict:
         return "DR"
+    else:
+        return "S"
 
-def distribute(strain,period,INH,RIF,ACK,FLUO,EMB,PZA):
+def distribute(strain,period,mutationDict):
     if period == "MDR":
         MDR_list.write(strain + "\n")
-        if PZA != "":
+        if mutationDict.get("PZA"):
             MDR_PZA_list.write(strain + "\n")
-        if EMB != "":
+        if mutationDict.get("EMB"):
             MDR_EMB_list.write(strain + "\n")
     elif period == "pre-XDR":
         pre_XDR_list.write(strain + "\n")
-        if PZA != "":
+        if mutationDict.get("PZA"):
             pre_XDR_PZA_list.write(strain + "\n")
-        if EMB != "":
+        if mutationDict.get("EMB"):
             pre_XDR_EMB_list.write(strain + "\n")
     elif period == "XDR":
         XDR_list.write(strain + "\n")
-        if PZA != "":
+        if mutationDict.get("PZA"):
             XDR_PZA_list.write(strain + "\n")
-        if EMB != "":
+        if mutationDict.get("EMB"):
             XDR_EMB_list.write(strain + "\n")
     elif period == "DR":
         DR_list.write(strain + "\n")
-        if RIF != "":
+        if mutationDict.get("RIF"):
             DR_RIF_list.write(strain + "\n")
-        if INH != "":
+        if mutationDict.get("INH"):
             DR_INH_list.write(strain + "\n")
-        if PZA != "":
+        if mutationDict.get("PZA"):
             DR_PZA_list.write(strain + "\n")
-        if EMB != "":
+        if mutationDict.get("EMB"):
             DR_EMB_list.write(strain + "\n")
     else:
         S_list.write(strain + "\n")
 
 def IREP_calculate(strain,period,INH,RIF,EMB,PZA):
-    if INH != "":
+    if INH != "['']":
         INH_mutation_spectrum.write("%s\t%s\t%s\n" %(strain,period,INH))
-    if RIF != "":
+    if RIF != "['']":
         RIF_mutation_spectrum.write("%s\t%s\t%s\n" %(strain,period,RIF))
-    if EMB != "":
+    if EMB != "['']":
         EMB_mutation_spectrum.write("%s\t%s\t%s\n" %(strain,period,EMB))
-    if PZA != "":
+    if PZA != "['']":
         PZA_mutation_spectrum.write("%s\t%s\t%s\n" %(strain,period,PZA))
 
-if __name__ == "main":
-    for strain in strainList:
-        strain = strain.rstrip()
-        MutationProcess(strain)
+for strain in strainList:
+    strain = strain.rstrip()
+    MutationProcess(strain)

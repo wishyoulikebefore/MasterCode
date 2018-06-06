faaf949da52df6c230e4d5851fc2203ab8a53902
wishyoulikebefore <37919975+wishyoulikebefore@users.noreply.github.com>
2018-06-06 15:23:34 +0800

    Update getAdapter_Kmer.py

--- fastqc/getAdapter_Kmer.py
+++ fastqc/getAdapter_Kmer.py
@@ -7,12 +7,14 @@ fileName = sys.argv[1]
 AdpSig = 0
 KmerSig = 0
 AdapterFile = open("tmp1","a")
-KmerFile = open("tmp2","a")
+Kmer_count = 0
 
 def trigStart_adapter(line):
         global AdpSig
         if "Adapter Content" in line:
                 AdpSig = 1
+                adapterJudgement = line.rstrip().split("\t")[1]
+                print("adapterJudgement: %s" %(adapterJudgement))
         return
 
 def trigEnd_adapter(line):
@@ -25,6 +27,8 @@ def trigStart_kmer(line):
         global KmerSig
         if "Kmer Content" in line:
                 KmerSig = 1
+                kmerJudgement = line.rstrip().split("\t")[1]
+                print("kmerJudgement: %s" %(kmerJudgement))
         return
 
 def trigEnd_kmer(line):
@@ -39,30 +43,17 @@ with open(fileName) as f:
                 trigEnd_adapter(line)
                 trigStart_kmer(line)
                 trigEnd_kmer(line)
-                if AdpSig:
+                if AdpSig and "Adapter Content" not in line:
                         AdapterFile.write(line)
-                if KmerSig:
-                        KmerFile.write(line)
+                if KmerSig and "Kmer Content" not in line:
+                        Kmer_count += 1
         AdapterFile.close()
-        KmerFile.close()
 
-fastqc_judgement = open("tmp1").readlines()[0].rstrip().split("\t")[1]
-print(fastqc_judgement)
-if len(open("tmp1").readlines()) == 3:
+if len(open("tmp1").readlines()) == 1:
         print("no adapter left")
 else:
-        dflist = []
-        columnsName = open("tmp1").readlines()[1].rstrip().split("\t")[1:]
-        indexName = []
-        for line in open("tmp1").readlines()[2:-1]:
-                lineOfList = line.rstrip().split("\t")
-                indexName.append(lineOfList[0])
-                dflist.append([float(i) for i in lineOfList[1:]])
-        df = pd.DataFrame(dflist,columns=columnsName,index=indexName)
+        df = pd.read_table("tmp1",sep="\t",index_col=0)
         df["sum"] = df.sum(axis=1)
         print(max(df["sum"]))
 os.remove("tmp1")
-
-kmer = len(open("tmp2").readlines())
-print(kmer-2)
-os.remove("tmp2")
+print("Kmer_count: %s" %(Kmer_count))                                     
